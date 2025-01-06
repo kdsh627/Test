048063173412eb2cb5133c16b782f9ed3cbca7df
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2025-01-07 00:56:11 +0900

    123

--- My project/Assets/NewMonoBehaviourScript1.cs
+++ My project/Assets/NewMonoBehaviourScript1.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class NewMonoBehaviourScript1 : MonoBehaviour
 {
-
-    //메롱
+    //응애
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("123");
+        
     }
 
     // Update is called once per frame
