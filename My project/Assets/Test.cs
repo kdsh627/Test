487679c78437dfc20cc36bb24f3e7308e3ee5ab2
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2025-01-09 10:23:39 +0900

    fdgfd
    fdg

--- My project/Assets/Test.cs
+++ My project/Assets/Test.cs
@@ -5,7 +5,7 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("123");
     }
 
     // Update is called once per frame
